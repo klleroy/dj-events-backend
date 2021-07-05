50ec37aa934d23fc39fa26a765acee88367d0a5e
Kevin LeRoy <kevin@kevinleroy.me>
2021-07-05 13:08:03 -0700

    Initial commit.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4d0c1ca71261f29a41139cf9ea994ca3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bacb9dbe-3a18-4746-ba72-808724c2dc49'
+};
